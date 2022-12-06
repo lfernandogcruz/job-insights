865da190602393e92c02943f9b932cefb6ed4189
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-12-06 16:59:25 -0300

    style: remove commented lines of implemented functions

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,18 +5,6 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path, encoding="utf-8") as read_file:
         reader = csv.DictReader(read_file, delimiter=",", quotechar='"')
         return list(reader)
@@ -24,20 +12,6 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     base_file = read(path)
     job_types = []
     for job in base_file:
