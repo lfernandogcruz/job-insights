88cfc74a4a44f3062d2ce320c54693dc4faf6473
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-12-07 00:53:17 -0300

    feat: req 05 - implements a function that filters the lowest paying job based on the salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -12,21 +12,12 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
+    base_file = read(path)
+    return min([int(job["min_salary"]) for job in base_file
+                if (job["min_salary"]).isdigit()])
     raise NotImplementedError
+# big shout out to instructor Maria Carolina and her brilliant
+# mentorship with the pokemon's get_min_attack example
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
