3d090a5c7af7e7678fabe84b58433ec1dda84e1f
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-12-15 13:54:06 -0300

    tests: req 11 - tests the brazilian_jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazuka_file = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for trampo in brazuka_file:
+        assert "title" in trampo
+        assert "salary" in trampo
+        assert "type" in trampo
